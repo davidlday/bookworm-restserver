69ea80b51dfcee86e6f48a6e5446632387a38a53
David L. Day <dday376@gmail.com>
2017-01-08 16:56:56 -0500

    Added /echo service.

--- src/main/java/com/prosegrinder/BookwormRestServer.java
+++ src/main/java/com/prosegrinder/BookwormRestServer.java
@@ -1,17 +1,39 @@
 package com.prosegrinder;
 
-import static spark.Spark.*;
 import com.prosegrinder.bookworm.util.Prose;
 import com.prosegrinder.bookworm.util.ReadabilityScores;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 public class BookwormRestServer {
     public static void main(String[] args) {
-        get("/info", (req, res) -> "Info!");
+        post("/echo", (request, response) -> {
+          response.status(200);
+          response.type("application/json");
+          return request.body();
+        });
+
+        post("/params", (req, res) -> {
+          return req.params();
+        });
+
+        post("/queryParams", (req, res) -> {
+          return req.queryParams();
+        });
+
+//         post("/cat", (req, res) -> {
+//           MultipartConfigElement multipartConfigElement = new MultipartConfigElement("/tmp");
+//           request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
+//           return req.params("text");
+//         });
 
         post("/analysis", (req, res) -> {
           Prose prose = new Prose(req.queryParams("prose"));
           ReadabilityScores scores = new ReadabilityScores(prose);
-          return "Analyzed!";
+          return prose.toString();
         });
 
         post("/extraction", (req, res) -> "Extracted!");
