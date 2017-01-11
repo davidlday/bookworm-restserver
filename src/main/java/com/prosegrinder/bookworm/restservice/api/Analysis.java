6ae9eb37ba785ddac77c99b6f94e6910dd8365dc
David L. Day <dday376@gmail.com>
2017-01-11 15:10:24 -0500

    Added more attributes to Analysis.java.

--- src/main/java/com/prosegrinder/bookworm/restservice/api/Analysis.java
+++ src/main/java/com/prosegrinder/bookworm/restservice/api/Analysis.java
@@ -2,9 +2,13 @@ package com.prosegrinder.bookworm.restservice.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.prosegrinder.bookworm.util.Prose;
+import com.prosegrinder.bookworm.util.Word;
 import com.prosegrinder.bookworm.util.ReadabilityScores;
 import io.dropwizard.jackson.JsonSnakeCase;
 import org.hibernate.validator.constraints.Length;
+
+import java.util.Map;
+import java.util.Set;
 
 @JsonSnakeCase
 public class Analysis {
@@ -29,6 +33,36 @@ public class Analysis {
   @JsonProperty
   public Integer getSyllableCount() {
     return prose.getSyllableCount();
+  }
+
+  @JsonProperty
+  public Map<Word, Integer> getWordFrequency() {
+    return prose.getWordFrequency();
+  }
+
+  @JsonProperty
+  public Set<Word> getUniqueWords() {
+    return prose.getUniqueWords();
+  }
+
+  @JsonProperty
+  public Integer getUniqueWordCount() {
+    return prose.getUniqueWordCount();
+  }
+
+  @JsonProperty
+  public Integer getSentenceCount() {
+    return prose.getSentenceCount();
+  }
+
+  @JsonProperty
+  public Integer getParagraphCount() {
+    return prose.getParagraphCount();
+  }
+
+  @JsonProperty
+  public String getPov() {
+    return prose.getPov().toString();
   }
 
 }
