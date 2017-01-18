62aa91edaeee5f7d4666b168758659e6037042c1
David L. Day <dday376@gmail.com>
2017-01-18 06:51:53 -0500

    Adding convenience class to match pov_indicator_frequency in original experimental api.

---
+++ src/main/java/com/prosegrinder/bookworm/restservice/api/PovIndicatorFrequency.java
@@ -0,0 +1,32 @@
+package com.prosegrinder.bookworm.restservice.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.prosegrinder.bookworm.restservice.api.WordFrequency;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PovIndicatorFrequency {
+  private final Integer totalFrequency;
+  private final List<WordFrequency> indicatorFrequency
+      = new ArrayList<WordFrequency>();
+
+  @JsonCreator
+  public PovIndicatorFrequency(@JsonProperty final Integer total,
+      @JsonProperty final List<WordFrequency> indicators) {
+    this.totalFrequency = total;
+    this.indicatorFrequency.addAll(indicators);
+  }
+
+  @JsonProperty
+  public final Integer getTotalFrequency() {
+      return this.totalFrequency;
+  }
+
+  @JsonProperty
+  public final List<WordFrequency> getIndicatorFrequency() {
+      return this.indicatorFrequency;
+  }
+
+}
