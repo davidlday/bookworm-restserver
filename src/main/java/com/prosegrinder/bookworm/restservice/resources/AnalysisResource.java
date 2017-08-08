543b386232aa7fe86ffa6aa451b3ee936c81eed9
David L. Day <dday376@gmail.com>
2017-08-08 07:24:36 -0400

    Removed unnecessary exceptions.

--- src/main/java/com/prosegrinder/bookworm/restservice/resources/AnalysisResource.java
+++ src/main/java/com/prosegrinder/bookworm/restservice/resources/AnalysisResource.java
@@ -4,8 +4,6 @@ import com.prosegrinder.bookworm.restservice.api.Analysis;
 import com.prosegrinder.bookworm.restservice.api.ProsePayload;
 import com.prosegrinder.bookworm.util.Dictionary2;
 
-import java.io.IOException;
-
 import com.codahale.metrics.annotation.Timed;
 
 // import java.util.Optional;
@@ -14,10 +12,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 // import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response.Status;
 
 @Path("/analysis")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -26,12 +22,8 @@ public class AnalysisResource {
 
   @POST
   @Timed
-  public final Analysis getAnalysis(@NotNull final ProsePayload prosePayload) throws IOException {
-    try {
-      return new Analysis(prosePayload.getText(), Dictionary2.getDefaultDictionary());
-    } catch (IOException ioe) {
-      throw new WebApplicationException(ioe.getMessage(), Status.NOT_FOUND);
-    }
+  public final Analysis getAnalysis(@NotNull final ProsePayload prosePayload) {
+    return new Analysis(prosePayload.getText(), Dictionary2.getDefaultDictionary());
   }
 
 }
